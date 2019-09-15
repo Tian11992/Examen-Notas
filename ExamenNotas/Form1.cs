ef3f77c947cbafaf2a85233d8e2b136f6029e594
Sebastian Correa <sebas.11992@hotmail.com>
2019-09-15 11:16:25 -0500

    Examen: programa que toma 5 notas

---
+++ ExamenNotas/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenNotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, nota4, nota5,promedio;
+
+            nota1 = Convert.ToDouble(txtnota1.Text);
+            nota2 = Convert.ToDouble(txtnota2.Text);
+            nota3 = Convert.ToDouble(txtnota3.Text);
+            nota4 = Convert.ToDouble(txtnota4.Text);
+            nota5 = Convert.ToDouble(txtnota5.Text);
+
+            promedio = ((nota1 + nota2 + nota3 + nota4 + nota5) / 5);
+
+            txtpromedio.Text = Convert.ToString(promedio);
+
+            if (promedio <= 1)
+            {
+                MessageBox.Show ("Reprueba la materia sin lograr realizar proceso de recuperación.");
+            }
+            else
+            {
+                if (promedio<=2.9)
+                {
+                    MessageBox.Show("Reprueba la materia y puede hacer la habilitación.");
+                }
+                else
+                {
+                    if (promedio<=3.9)
+                    {
+                        MessageBox.Show("Aprueba la materia con plan de mejora.");
+                    }
+                    else
+                    {
+                        if (promedio<=4.5)
+                        {
+                            MessageBox.Show("Buen rendimiento.");
+                        }
+                        else
+                        {
+                            if (promedio<=4.9)
+                            {
+                                MessageBox.Show("Excelente.");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Graduado con honores.");
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ ExamenNotas/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("ExamenNotas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ExamenNotas")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3668f87d-3a54-40c6-8b8b-14c1b773a08c")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
